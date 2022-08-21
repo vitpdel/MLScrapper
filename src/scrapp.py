fc78f783c276bd4eb4d7b00bf2ab97e44206f1d8
Vitor Santos <vitorpedrosasantos@gmail.com>
2022-08-21 18:59:47 -0300

    Added comments

--- src/scrapp.py
+++ src/scrapp.py
@@ -1,32 +1,36 @@
-from turtle import title
+from itertools import product
+import  os, sys
 from    bs4 import  BeautifulSoup
 import  requests
 
-def scrap():
-    """
-    Scrapes the name and the price of the product of the page.
-    """
 
-    URL =   input("Please, enter the url of the product: ")
+def scrap():
+    URL =   input("Please, enter the product url: ")
+    
+    #This makes scraping work with any browser
     headers =   {'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.102 Safari/537.36'}
 
     html  =   requests.get(URL,    headers=headers)
     soup    =   BeautifulSoup(html.text, "html.parser")
 
     try:
-        title=  soup.find("h1", class_  =   "ui-pdp-title").get_text().strip()
+        #Search prices and product names through HTML
+        title   =   soup.find("h1", class_  =   "ui-pdp-title").get_text().strip()
 
         moneySymbol =   soup.find("span",   class_  =   "andes-money-amount__currency-symbol").get_text()
         price   =  soup.find("span", class_    =   "andes-money-amount__fraction").get_text().strip()
         dec =   soup.find("span",   class_  =   "andes-money-amount__cents andes-money-amount__cents--superscript-36").get_text().strip()
-
+    
+        print("\n Product:")
         print(title)
+        print("\n Price:")
         print(moneySymbol, price + "," + dec)
         
     except  AttributeError:
-        moneySymbol =   soup.find("span",   class_  =   "andes-money-amount__currency-symbol").get_text()
-        price   =  soup.find("span", class_    =   "andes-money-amount__fraction").get_text().strip()
+        #If the product price has no decimal places
+        print("\n Product:")
         print(title)
+        print("\n Price:")
         print(moneySymbol, price)
 
     
